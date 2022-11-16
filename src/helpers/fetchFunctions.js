fa10a9a11787ba9f69fea32631c6f07bbf4d473e
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-16 19:07:59 -0300

    resolvendo requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,14 @@
 export const fetchProduct = () => {
   // seu código aqui
+
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async (resultados) => {
   // seu código aqui
+  if (resultados) {
+    const resu = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${resultados}`);
+    const response = await resu.json();
+    return response.results;
+  }
+  throw new Error('Termo de busca não informado');
 };
