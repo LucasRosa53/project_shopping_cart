c739f9c1bd5aaabaedb3f3161fccb1c95bd6c316
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-16 21:38:21 -0300

    resolvido requisito 2 e fazendo requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
+    expect(typeof fetchProductsList).toBe('function')
+  });
+
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    // const fetch = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY'
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalled()
+  });
+
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  });
+
+  it('este se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const esperar = await (fetchProductsList('computador'))
+    expect(esperar).toEqual(computadorSearch)
+  });
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: Termo de busca não informado', () => {
     
   });
-
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
-  });
-
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
-  });
-
-  // it('...', () => {
-  // });
 });
