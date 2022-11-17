1293cc97a994a7c072c5d5e7a4979c360e5e5d25
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-17 16:56:08 -0300

    resolvido requisito 3

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -8,6 +8,7 @@ export const fetchProductsList = async (resultados) => {
   if (resultados) {
     const resu = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${resultados}`);
     const response = await resu.json();
+    console.log(response);
     return response.results;
   }
   throw new Error('Termo de busca não informado');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
 import './style.css';
+import { createProductElement } from './helpers/shopFunctions';
+
+const listaProdutos = document.querySelector('.products');
+
+const funcaoGeral = async () => {
+  const funcaoListaProdutos = await fetchProductsList('computador');
+  funcaoListaProdutos.forEach((element) => {
+    listaProdutos.appendChild(createProductElement(element));
+  });
+};
+funcaoGeral();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 // INICIANDO PROJETO, VAMO QUE VAMO!
