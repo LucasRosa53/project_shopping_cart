f178c30f49da0c515f85589907a7e696f471ceea
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-17 18:16:27 -0300

    resolvido requisito 4

--- src/main.js
+++ src/main.js
@@ -5,13 +5,26 @@ import { createProductElement } from './helpers/shopFunctions';
 
 const listaProdutos = document.querySelector('.products');
 
+const loadPag = () => {
+  const mensageCarregando = document.createElement('span');
+  mensageCarregando.classList.add('loading');
+  mensageCarregando.innerHTML = 'carregando...';
+  listaProdutos.appendChild(mensageCarregando);
+};
+
+const removedorDeMensage = () => {
+  const remove = document.querySelector('.loading');
+  remove.remove();
+};
 const funcaoGeral = async () => {
   const funcaoListaProdutos = await fetchProductsList('computador');
   funcaoListaProdutos.forEach((element) => {
     listaProdutos.appendChild(createProductElement(element));
   });
 };
-funcaoGeral();
+loadPag();
+await funcaoGeral();
+removedorDeMensage();
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 // INICIANDO PROJETO, VAMO QUE VAMO!
