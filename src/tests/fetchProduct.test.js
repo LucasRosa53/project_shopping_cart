ad0033367964a372a6cec8c5006f04b9dec3a409
LucasRosa53 <lucasrosa223@hotmail.com>
2022-11-21 15:26:53 -0300

    resolvendo requisito 6 e começando o 7

--- src/tests/fetchProduct.test.js
+++ src/tests/fetchProduct.test.js
@@ -4,7 +4,22 @@ import product from './mocks/product';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProduct', () => {
-  it('...', () => {
-    
+  it('Teste se fetchProduct é uma função', () => {
+    expect(typeof fetchProduct).toBe('function')
+  });
+  it('Execute a função fetchProduct com o argumento do produto MLB1405519561 e teste se fetch foi chamada', async () => {
+    await fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalled()
+  });
+  it('Teste se, ao chamar a função fetchProduct com o argumento do produto MLB1405519561, a função fetch utiliza o endpoint https://api.mercadolibre.com/items/MLB1405519561', async () => {
+    await fetchProduct('MLB1405519561')
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1405519561')
+  });
+  it('este se o retorno da função fetchProduct com o argumento do produto MLB1405519561 é uma estrutura de dados igual ao objeto produto que já está importado no arquivo.', async () => {
+    await fetchProduct('MLB1405519561')
+    expect(fetch).toEqual('produto')
+  });
+  it('Teste se, ao chamar a função fetchProduct sem argumento, retorna um erro com a mensagem: ID não informado', async () => {
+    expect(fetchProduct()).rejects.toThrow('ID não informado')
   });
 });
